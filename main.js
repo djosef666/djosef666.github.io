246fa7b554a3eea3abe347a3e379e6f11bfa5480
djosef <117583199+djosef666@users.noreply.github.com>
2022-11-07 15:00:07 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('hardcore');
+
+// document.querySelector("audio").play()
+
+var links = document.querySelectorAll("a")
+console.log(links)
+
+function playburp() {
+    console.log('playburp')
+    document.querySelector("audio").play()
+}
+document.querySelector("body").addEventListener('click', playburp)
